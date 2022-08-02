aede5962ae1bd0b869a89ead4d2a28447c43f82f
Lemanyaqubova <109238717+Lemanyaqubova@users.noreply.github.com>
2022-08-02 18:24:36 +0400

    Add files via upload
    homework2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            int m = 21;
+            int count = 0;
+                for (int i = n; i <= m; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    count++;
+                        Console.WriteLine(count);
+                }
+                {
+
+                }
+            }
+            {
+
+            }
+        }
+    }
+}
